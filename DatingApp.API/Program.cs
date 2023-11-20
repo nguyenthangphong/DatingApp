c78bb92759cc14e7bffabf653fdb56341b5af114
nguyenthangphong <nthangphong2601@gmail.com>
2023-11-21 01:18:08 +0700

    Second commit

--- DatingApp.API/Program.cs
+++ DatingApp.API/Program.cs
@@ -10,13 +10,16 @@ builder.Services.AddDbContext<DataContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
+app.UseCors(builder => builder
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .WithOrigins("https://localhost:4200")
+);
 
 app.MapControllers();
 
