04d1b01309699563ba2e1aa2695b6ab42934f7d2
phongbruno <thangphong8755@gmail.com>
2023-11-20 21:11:09 +0700

    First commit

---
+++ DatingApp.API/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using DatingApp.API.Data;
+using DatingApp.API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")] // /api/user
+public class UserController : ControllerBase
+{
+    private readonly DataContext _context;
+
+    public UserController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        return await _context.Users.ToListAsync();
+    }
+
+    [HttpGet("{id}")] // /api/user/2
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+        return await _context.Users.FindAsync(id);
+    }
+}
