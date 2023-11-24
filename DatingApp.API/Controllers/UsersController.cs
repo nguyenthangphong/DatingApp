0a9a95991f5840663c937f98f6c806c78ebced89
nguyenthangphong <nthangphong2601@gmail.com>
2023-11-25 00:37:23 +0700

    Commit section 10

--- DatingApp.API/Controllers/UsersController.cs
+++ DatingApp.API/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
-﻿using AutoMapper;
+﻿using System.Security.Claims;
+using AutoMapper;
 using DatingApp.API.DTOs;
 using DatingApp.API.Interfaces;
 using Microsoft.AspNetCore.Authorization;
@@ -30,6 +31,17 @@ namespace DatingApp.API.Controllers
         public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
             return await _userRepository.GetMemberAsync(username);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var user = await _userRepository.GetUserByUsernameAsync(username);
+            if (user == null) return NotFound();
+            _mapper.Map(memberUpdateDto, user);
+            if (await _userRepository.SaveAllAsync()) return NoContent();
+            return BadRequest("Failed to update user");
         }
     }
 }
